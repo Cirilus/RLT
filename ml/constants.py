00dc8134b68fbf0edbb5d0bc32a02f1008d651c9
PocketBrain <tim.kirp@mail.ru>
2024-04-13 11:32:42 +0300

    fix model PATH

--- ml/constants.py
+++ ml/constants.py
@@ -1,6 +1,6 @@
 MODEL_EMB_NAME = "intfloat/multilingual-e5-large-instruct"
 DEVICE = "cuda"
-MODEL_CHAT_NAME = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
+MODEL_CHAT_NAME = "RLT/model-q8_0.gguf"
 SYSTEM_PROMPT = "Ты — Ассистент для Мегафон, ты отвечаешь на их вопросы и помогаешь им."
 
 
